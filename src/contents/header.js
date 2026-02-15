21837c63b55819c17515b9d327fbfa2537a34756
nichapa67 <nichapa.du@ku.th>
2026-02-15 17:33:46 +0700

    update code and fix imports

--- src/contents/header.js
+++ src/contents/header.js
@@ -1,15 +1,7 @@
-/*import fileName from '../assets/cv.pdf';
-export const data ={
-    "name": "Suriya Pinitkan",
-    "title": "Software Developer · between jobs",
-    "caption": "From concept to reality, bridging the gap with code, transforming ideas into tangible digital solutions.",
-    btnText: "View Resume",
-    link: fileName
-}*/
 import filename from '../assets/CV.pdf';
 
 export const data = {
-name:"Suriya Pinitkan",
+name:"Nichapa Dumripattanasakul",
 title:"Software Developer · between jobs",
 caption:"From concept to reality, bridging the gap with code, transforming ideas into tangible digital solutions.",
 btnText:"View Resume",
